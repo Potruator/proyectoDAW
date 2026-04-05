c7b4c4a5643258cfeefb4a9a5d5b177b223e7e3b
Potruator <franviolin@gmail.com>
2026-04-05 20:02:54 +0200

    created controller

---
+++ app/Http/Controllers/Client/ClientDashboardController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+use App\Models\UserOffer;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ClientDashboardController extends Controller
+{
+    public function index() {
+        $offers = UserOffer::with('offer')
+            ->where('user_id', auth()->id())
+            ->whereNull('redeemed_at') // Solo las no canjeadas
+            ->get()
+            ->map(function ($item) {
+                return [
+                    'uuid' => $item->uuid, // ID único del qr
+                    'title' => $item->offer->title, // Viene de la relación
+                    'description' => $item->offer->description, // También viene de la relación
+                    'assigned_at' => $item->assigned_at->diffForHumans()
+                ];
+            });
+
+        return Inertia::render('Client/Dashboard', [
+            'offers' => $offers
+        ]);
+    }
+
+    public function show(UserOffer $userOffer) {
+
+        // Nos aseguramos e que no se roben QRs ajenos
+        if ($userOffer->user_id !== auth()->id()) {
+            abort(403);
+        }
+
+        return Inertia::render('Client/Offers/Show', [
+            'userOffer' => [
+                'uuid' => $userOffer->uuid,
+                'title' => $userOffer->offer->title
+            ]
+        ]);
+    }
+}
