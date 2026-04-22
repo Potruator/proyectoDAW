15a40f096193577d641387bc636d008fd048f226
Potruator <franviolin@gmail.com>
2026-04-23 01:20:48 +0200

    fix bug -> faltaba la prop products

--- resources/js/Pages/Admin/Offers/Create.jsx
+++ resources/js/Pages/Admin/Offers/Create.jsx
@@ -2,14 +2,18 @@ import AppLayout from '@/Layouts/AppLayout';
 import OfferForm from '@/Components/OfferForm';
 import { Head } from '@inertiajs/react';
 
-export default function Create() {
+export default function Create({ products }) {
     return (
         <AppLayout>
             <Head title='Crear Oferta'/>
-            <div className='w-full'>
+            <div className='max-w-3xl mx-auto'>
                 <h1 className='text-3xl font-bold text-white mb-6'>Crear nueva oferta</h1>
                 <div className='bg-gray-900 rounded-lg p-8 border border-gray-800'>
-                    <OfferForm submitUrl='/app/admin/offers' submitText='Crear Oferta' isEditing={false}/>
+                    <OfferForm 
+                        submitUrl='/app/admin/offers' 
+                        submitText='Crear Oferta'
+                        products={products}
+                        isEditing={false}/>
                 </div>
             </div>
         </AppLayout>
