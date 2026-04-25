fdae0033a14d1cba0a01eaaecca4816a3d04ea5a
Potruator <franviolin@gmail.com>
2026-04-25 19:54:32 +0200

    added ES password reponses

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    // Líneas de Idioma para Restablecimiento de Contraseñas
+    'reset' => '¡Tu contraseña ha sido restablecida!',
+    'sent' => '¡Te hemos enviado por correo el enlace para restablecer tu contraseña!',
+    'throttled' => 'Por favor espera antes de intentar de nuevo.',
+    'token' => 'El token de restablecimiento de contraseña es inválido.',
+    'user' => "No podemos encontrar un usuario con esa dirección de correo electrónico."
+];
